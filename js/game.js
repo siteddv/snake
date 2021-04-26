0a938708af4d0b77549e801c389a9a4a9716b416
Dimitrii Vinogradov <siteddv@gmail.com>
2021-04-26 13:36:36 +0600

    added food & snake objects

--- js/game.js
+++ js/game.js
@@ -12,8 +12,11 @@ const score = 0;
 
 let foodCoordinates = {
    x: Math.floor(Math.random() * 17 + 1) * boxSize,
-   x: Math.floor(Math.random() * 15 + 1) * boxSize,
+   y: Math.floor(Math.random() * 15 + 1) * boxSize,
 }
+
+let snakeBodyArray = [];
+snakeBodyArray.push({ x: 9 * boxSize, y: 10 * box });
 
 function drawGame() {
    context.drawImage(background, 0, 0);
